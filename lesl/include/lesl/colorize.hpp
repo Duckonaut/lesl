95da114168e9c22ead36f0c64dd59c0e33992147
Duckonaut <duckonaut@gmail.com>
2026-05-12 15:58:00 +0200

    update assimp, small fixes

--- lesl/include/lesl/colorize.hpp
+++ lesl/include/lesl/colorize.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ostream>
+#include <concepts>
 
 #define COLOR_ENABLE 1
 
--- lesl/include/lesl/tracking_allocator.hpp
+++ lesl/include/lesl/tracking_allocator.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <cstdint>
+#include <cstdio>
 
 template <typename T> struct TrackingAllocator;
 
